5e1550489b9c8453c0701d0c2f3f4104cad71a4b
Kieran Meteyard <kieranmeteyard@Kierans-MacBook-Air.local>
2024-01-11 10:34:04 +0000

    2022 day 1 and tidying

--- 2022/day/1/script.R
+++ 2022/day/1/script.R
@@ -1,17 +1,22 @@
-library(aochelpers)
-# input <- aoc_input_vector(1, 2022) # uncomment at end, once correct on test input
-# also consider aoc_input_data_frame() or aoc_input_matrix(), with view = TRUE
 
-browseURL("https://adventofcode.com/2022/day/1")
-# use datapasta addin to copy in test input from the website
-# triple-click in test input box in the puzzle to select, 
-# then choose appropriate paste format from addin
-# comment out once ready to run on full input
+input <- readLines('input.txt')
 
-# input <- 
+input <- as.numeric(input)
 
 # Part 1 ---------------------------------------------------------------------
-
+totals = c(0)
+current = 0
+for (i in aoc_input) {
+  if (is.na(i)) {
+    totals = append(totals, current)
+    current = 0
+    next
+    
+  }
+    current = current + i
+}
+max(totals)
 
 
 # Part 2 ---------------------------------------------------------------------
+sum(sort(totals, decreasing = TRUE)[1:3])
